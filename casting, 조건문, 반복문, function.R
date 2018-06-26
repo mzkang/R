04579acfaba4e36a99581cce2cc4c40953375d3e
mzkang <0911kmj@naver.com>
2018-06-26 12:49:42 +0900

    2018-06-26

---
+++ casting, 조건문, 반복문, function.R
@@ -0,0 +1,172 @@
+###Casting###
+#object type의 전환
+
+a = matrix(1:10,5,2)
+a[c(1,3,5),]
+a[c(T,F,T,F,T),] #df도 가능한 indexing
+
+
+##matrix -> vector
+c(a) 
+str(c(a))
+drop(a[,1,drop=F])     
+str(a[,1,drop=F])
+
+
+##df -> matrix
+a = data.frame(1,2)
+a
+b = as.matrix(a)
+b
+class(b)
+
+a = data.frame(v1 = 1,v2 = "a")
+a
+b = as.matrix(a)
+class(b)
+b  # 오토캐스팅 : 1이 chr가 됨(matrix는 원소의 type이 모두 같아야 함)
+
+
+##colnames, rownames
+#matrix의 colname, rowname변경
+a = matrix(1:10, 5,2)
+colnames(a) <- c('x1','x2')
+a
+rownames(a)
+rownames(a) <- c('r1','r2','r3','r4','r5')
+a
+ #r1~r5와 같이 연속된 숫자 쉽게 만들기
+paste0('r',1:5) #paste0 = paste(...,sep = '')
+rownames(a) <- paste0('a',1:5)
+a
+#df의 colname, rowname변경
+b = as.data.frame(a)
+colnames(b)
+names(b) #df = list라서 list의 이름을 알아내는 방법과 동일
+names(b) <- c('v1','v2')
+names(b)
+
+
+##df -> list
+b = unclass(b)
+class(b)
+b
+
+
+
+
+
+
+##조건문, 반복문
+x=0
+for (i in 1:3){
+  x = x + i
+  }
+x
+i
+
+x=0
+for (i in 1:3){     # outer loop
+  for (j in 1:2){   # inner loop
+    x = x + i + j
+  }
+  print(x)
+}
+
+
+## control the loop
+# next
+x=0
+for (i in 1:5){
+  x= x+i
+  if (i %% 2 == 1){
+    next} 
+  print(x)
+  }
+  
+
+#hw : stop, break
+
+
+
+
+
+
+
+
+
+
+##function : 코드의 양을 줄이려고 만듦
+##사용자 정의 함수
+test.f <- function(x1,x2)
+{
+  v = x1^2 + x2  # 함수를 만들 때엔 선언된 변수만 사용할 것을 권함 
+  return(v)      
+}
+print(test.f) # 함수 내용 보기
+
+x1=1
+x2=2
+test.f(x1=3, x2=4)
+x1    # 함수에서 정의된 변수는 함수 안에서만 유의미함
+x2
+test.f(x1,x2)
+test.f(x2,x1)
+test.f(x1 = x2, x2 = x1) # 앞쪽 변수는 함수 안에서의 변수(위치를 지정) 
+                         # / 뒤쪽은 일반적 변수(global variable)
+test.f(x1 = 2, x2 = 1)
+
+
+##return은 break, stop의 역할이 포함되어 있음
+# return하고 끝
+test.ff <- function(x1,x2)
+{
+  v1 = x1^2 + x2
+  v2 = x1^2 - x2
+  return(c(v1,v2))
+  v3 = 3
+  return(v3)
+}
+test.ff(3,1) #v3는 return 안됨. 앞에 return 실행되고 끝남
+
+
+# example : column average function
+scol_mean <- function(x)
+{
+  if (class(x) != 'matrix') 
+    stop("x is not matrix")
+  v = rep(0, ncol(x))
+  for (i in 1:ncol(x)) 
+  {v[i] = mean(x[,i])}
+return(v)
+}
+
+x= matrix(1:10,5,2)
+scol_mean(x)
+x= matrix(1:30,6,5)
+
+
+
+
+##path
+CO2
+View(CO2)
+getwd()
+setwd('./R')
+save.image('저장할 이름')
+load('불러올 파일')
+
+##CO2 데이터 csv로 저장하기, 불러오기
+write.csv(CO2, file = 'CO2.csv')
+write.csv(CO2, file='CO2.csv',
+          row.names = F)
+str(CO2)
+coco2 <- read.csv('CO2.csv', header = T)
+str(coco2)
+
+
+
+
+
+
+
